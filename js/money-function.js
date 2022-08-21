bb39cc1d692b93bcef7a0c944742a742d2f5792b
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-08-21 18:31:56 +0600

    Function & Validation Added

--- js/money-function.js
+++ js/money-function.js
@@ -2,7 +2,9 @@ function getInputValueById(elementId) {
     const inputField = document.getElementById(elementId)
     const inputTotalString = inputField.value
     const inputTotal = parseInt(inputTotalString)
+
     return inputTotal;
+
 }
 
 function setValueById(elementId, value) {
@@ -16,7 +18,23 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
     const foodTotal = getInputValueById('food-field');
     const clothsTotal = getInputValueById('cloths-field');
 
+    if (isNaN(incomeTotal && rentTotal && foodTotal && clothsTotal)) {
+        alert('Please Provide a Valid Input!!')
+        return
+    }
+
+    if (incomeTotal && rentTotal && foodTotal && clothsTotal < 0) {
+        alert('Please Provide a Valid Positive Number!!')
+        return
+    }
+
+
     const totalExpenses = rentTotal + foodTotal + clothsTotal;
+
+    if (totalExpenses > incomeTotal) {
+        alert('You Can Not Expense More Than Your Income!!')
+        return;
+    }
 
     setValueById('expenses-total', totalExpenses)
 
@@ -29,13 +47,25 @@ document.getElementById('savings-btn').addEventListener('click', function () {
 
     const incomeTotal = getInputValueById('income-field');
     const savingsElement = getInputValueById('save-field')
-    const savingsAmount = (savingsElement / 100) * incomeTotal;
 
-    setValueById('saving-total', savingsAmount);
+    if (savingsElement >= 100) {
+        alert('Saving Amount Limit is Not More Than 100%!!')
+        return
+    }
+
+    const savingsAmount = (savingsElement / 100) * incomeTotal;
 
     const previousBalanceTotal = document.getElementById('balance-total')
     const previousBalanceString = previousBalanceTotal.innerText
     const previousBalance = parseInt(previousBalanceString);
+
+    if (savingsAmount > previousBalance) {
+
+        alert('You Can Not Save More Than Your Balance!!')
+        return;
+    }
+
+    setValueById('saving-total', savingsAmount);
 
     const remainingBalanceAmount = previousBalance - savingsAmount;
 
